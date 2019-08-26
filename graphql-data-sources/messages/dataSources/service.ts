50bfc0b19724e649fe9c391a7b199cff5e7e50f8
philipsales <philip@alliedworld.healthcare>
2019-08-26 20:26:08 +0800

    Add logger library

--- graphql-data-sources/messages/dataSources/service.ts
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-const { RESTDataSource } = require('apollo-datasource-rest');
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 'use strict';
-//const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools');
-const { ApolloServer, 
-  addSchemaLevelResolveFunction,
-  makeExecutableSchema } = require('apollo-server');
+require('./server/config/config');
 
-const typeDefs = require('./schema'); 
-const resolvers = require('./resolver');
-const LaunchAPI = require('./datasource/launch');
-const MessageAPI = require('./datasource/message');
-const ResidentAPI = require('./datasource/resident');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./server/schema'); 
+const resolvers = require('./server/resolver');
+
+const LaunchAPI = require('./datasource/launch/launch');
+const MessageAPI = require('./datasource/message/message');
+const ResidentAPI = require('./datasource/resident/resident');
+
+const log = require('./server/lib/logger/logger');
+const logger = log.logger.child({ sourceFile: log.file.setFilename(__filename) });
 
 const dataSources = () => ({
   launchAPI: new LaunchAPI(),
@@ -16,37 +18,24 @@ const dataSources = () => ({
   residentAPI: new ResidentAPI()
 });
 
-const logger = { log: e => console.log(e) }
-
-//const schema = makeExecutableSchema({ typeDefs });
-/*
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-  logger
-});
-*/
-
 const rootResolveFunction = (parent, args, context, info) => {
   //perform action before any other resolvers
 };
 
-// addSchemaLevelResolveFunction(schema, rootResolveFunction)
-
 const server = new ApolloServer({ 
     typeDefs, 
-    //schema,
     dataSources,
     resolvers,
     tracing: true,
     cacheControl: false,
     engine: {
-      apiKey: 'ENGINE_API_KEY=service:awh-notifications:AaQg0qq5n8DbaGVNuX1iAQ',
+      apiKey: process.env.APOLLO_ENGINE_KEY,
     },
 });
 
 if (process.env.NODE_ENV !== 'test'){
   server.listen({ port: 4001 }).then(({ url }) => {
+    logger.info('🚀 initialize server');
     console.log(`🚀 app running at ${url}`);
   });
 }
